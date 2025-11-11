e2247c4a31bb762cc233ccaba521f12e1f8a454d
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2025-11-11 02:08:35 -0800

    end pipe session

--- src/pipes/validate/mobile/mobile.pipe.ts
+++ src/pipes/validate/mobile/mobile.pipe.ts
@@ -7,18 +7,19 @@ import {
 
 @Injectable()
 export class MobilePipe implements PipeTransform {
-  constructor(private readonly mobileLength: number){}
+  constructor(private readonly mobileLength: number) {}
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: any, _metadata: ArgumentMetadata) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    const mobile = value.mobile;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const mobile: string = value.mobile;
+
     if (mobile.length !== this.mobileLength) {
       throw new BadRequestException('validation failed!');
     }
 
-    if (mobile.startsWith("0912")) {
-      value.operator = "hamrah aval"
+    if (mobile.startsWith('0912')) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      value.operator = 'hamrah aval';
     }
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return value;
